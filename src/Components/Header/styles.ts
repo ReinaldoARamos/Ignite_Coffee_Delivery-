9ddbbbfd0c20417e0b1e8891f5f0c31a80b48fc0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-23 17:24:41 -0300

    criando o tema e a cor do ícone e além disso também apliquei uma validação que caso a variant seja === purple ele troca o valor do ícone para brand-purple

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -21,24 +21,30 @@ export const HeaderButtonContainer = styled.div`
   gap: 0.75rem;
 `;
 
-interface HeaderButtonProps{
-    variant: "purple" | "yellow"
-   
-   }
-   
+interface HeaderButtonProps {
+  variant: "purple" | "yellow";
+}
+
 export const HeaderButton = styled.button<HeaderButtonProps>`
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   gap: 4px;
-   min-width: 2.3rem;
-   height: 2.3rem;
-   border: none;
-   padding: 0 0 0.5rem;
-   position: relative;
-   font-size: ${({ theme }) => theme.textSizes["text-regular-s"]};
-    
-    ${({variant, theme})=> css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  min-width: 2.3rem;
+  height: 2.3rem;
+  border: none;
+  padding: 0 0 0.5rem;
+  position: relative;
+  font-size: ${({ theme }) => theme.textSizes["text-regular-s"]};
+
+  ${({ variant, theme }) => css`
     background: ${theme.colors[`brand-${variant}-light`]};
+    color: ${theme.colors[`brand-${variant}-dark`]};
+  `}
+
+  ${({ variant, theme }) =>
+    variant === "purple" &&
+    css`
+      color: ${theme.colors[`brand-purple`]};
     `}
 `;
