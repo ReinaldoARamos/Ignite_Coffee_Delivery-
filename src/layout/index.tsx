ac408d031e70bb146674db037c763fe51175df22
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-23 15:22:05 -0300

    jogando o Header no defaultLayout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../Components/Header";
 import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
-    return (
-        <LayoutContainer>
-          <h1>Header</h1>
-            <Outlet/>
-        </LayoutContainer>
-    )
+  return (
+    <LayoutContainer>
+     <Header/>
+      <Outlet />
+    </LayoutContainer>
+  );
 }
