20bb7f3ce62dd458277eb80065fcd8e3394e541e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-24 14:05:47 -0300

    criando a mesma coisa que o title text mas para textos regulares, ou seja, de conteúdo

--- src/Components/Header/Typography.ts
+++ src/Components/Header/Typography.ts
@@ -4,9 +4,29 @@ import styled from 'styled-components'
 interface TitleTextPros{
 
     color?: "title" | "subtitle" | "text",
-    size?: "xl" | "l" | "m" | "s" | "xs"
+    size?: "xl" | "l" | "m" | "s" | "xs",
+    weight?: string | number
  }
+
+ interface RegularTextPros{
+
+    color?: "subtitle" | "text" | "label",
+    size?:  "l" | "m" | "s",
+    weight?: string | number
+ }
+
 export const TitleText = styled.h1<TitleTextPros>`
 color: ${({theme, color})=> theme.colors[`base-${color ?? "title"}`]};
 font-size: ${({theme, size})=> theme.textSizes[`title-title-${size ?? "m"}`]};
-`
+font-family: ${({theme})=> theme.fonts.title};
+line-height: 130%;
+font-weight: ${({weight})=> weight ?? 800};
+`;
+
+export const RegularText = styled.p<RegularTextPros>`
+color: ${({theme, color})=> theme.colors[`base-${color ?? "text"}`]};
+font-size: ${({theme, size})=> theme.textSizes[`title-title-${size ?? "m"}`]};
+font-family: ${({theme})=> theme.fonts.title};
+line-height: 130%;
+font-weight: ${({weight})=> weight ?? 800};
+`;
