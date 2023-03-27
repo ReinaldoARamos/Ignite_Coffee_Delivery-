17cfd43ed02ce08dd125daff00afa6a66c2de350
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-27 15:51:08 -0300

    terminando de fazer a página Home

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -7,7 +7,10 @@ export const HeaderLayout = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
+  position: sticky ;
+  top: 0;
+  left:  0;
+  z-index: 5
   > div {
     width: 100%;
     display: flex;
