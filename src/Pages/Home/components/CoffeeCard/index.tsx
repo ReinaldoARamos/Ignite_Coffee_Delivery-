5bcfbb66e15c41b4b537f3e25819fc04cc1cf5d0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-27 15:27:47 -0300

    tipei o coffeeCard com coffeProps que recebe Coffee que é  a interface que recebe os dados do coffee.ts, assim, consigo fazer um map e retornar a lista com os dados

--- src/Pages/Home/components/CoffeeCard/index.tsx
+++ src/Pages/Home/components/CoffeeCard/index.tsx
@@ -15,23 +15,26 @@ import {
 
 
 export interface Coffee{
-id: number,
-tags: string[],
-name:string,
-description: string,
-photo: string,
-price: number
+id: number;
+tags: string[];
+name:string;
+description: string;
+photo: string;
+price: number;
 }
+
 interface CoffeeProps{
   coffee: Coffee
 }
 
-export function CoffeeCards({coffe} : CoffeeProps) {
+export function CoffeeCards({coffee} : CoffeeProps) {
   return (
     <CoffeeCardContainer>
       <img src="https://s3-alpha-sig.figma.com/img/b6af/775d/aaa94dcb37dfe1ee6535aae5b9356e9a?Expires=1680480000&Signature=GvkI9Vjxl4oI5bYje4ZH-vkjr7mG-vj1hiwpmLYR2PcTKd0mW1sC5OfwJCh-LkUyCY0BW218JYVwIRfQpgEoxZx4o7cXup8EjAL3V~ge-JnS98n~JSeL35I6Rm5kcUa7sl60iFVfjt0jMP4jDhxLIzoJWV7f0805Jsy7jLQzKaR9Rpbav6XLa1EcFa9RbQtkSH-6w2oMwFXd6KdrhcTTKRB4tLWteYrgp5TMi7Z8lqjftLQAvktKb1Ge63DKMY8n~xMsn2tlf0YV58oYAwO-2UwGHvKakBK6fMdd4rb-8mcNkfDJ4l2zTGi86-EgVSkI1LOynJx2e8Df-Cg45D1ohg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
       <Tags>
-        <span>Tradicional</span> <span>Com Leite</span>
+      {coffee.tags.map((tag)=>(
+            <span key={`${coffee.id}${tag}`}>{tag}</span> //a gente passa primeiro a id para nao conflitar as tags
+          ))}
       </Tags>
 
       <Name>Expresso Tradicional</Name>
