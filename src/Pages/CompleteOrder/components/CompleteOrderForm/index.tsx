5b761c6490aadfc39202e14079a0d653324b7cc8
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-27 17:36:45 -0300

    criando o componente do titulo e subtitulo do formulário e estilizando o mesmo

--- src/Pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,18 +1,25 @@
+import { MapPin } from "phosphor-react";
 import { TitleText } from "../../../../Components/Header/Typography";
 import { FormSectionContainer } from "../../styles";
+import { SectionTitle } from "../SectionTitle";
 import { CompleteOrderFormContainer } from "./style";
-
-
+import { useTheme } from "styled-components";
 export function CompleteOrderForm() {
-    return(
-<CompleteOrderFormContainer>
-    <TitleText size="xs" color="subtitle">
+  const colors = useTheme()
+    return (
+    <CompleteOrderFormContainer>
+      <TitleText size="xs" color="subtitle">
         Complete seu pedido
-    </TitleText>
-    <FormSectionContainer>
-        
-    </FormSectionContainer>
-</CompleteOrderFormContainer>
-
-    )
+      </TitleText>
+      <FormSectionContainer >
+        <SectionTitle
+          
+          icon={<MapPin size={22} weight="fill" color={colors.colors["brand-yellow-dark"]}/>}
+          title="Endereço de entrega"
+          subtitle="Informe o endereço de entrega"
+          
+        />
+      </FormSectionContainer>
+    </CompleteOrderFormContainer>
+  );
 }
