34914a15c75505196d0e03ea6bff2ee45ee32929
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-28 08:24:04 -0300

    colocando um grid com repeat 3 pq achei melhor

--- src/Pages/Home/components/OurCoffes/styles.ts
+++ src/Pages/Home/components/OurCoffes/styles.ts
@@ -8,7 +8,7 @@ margin-top: -10rem ;
 export const CoffeeList = styled.div`
 width: 100% ;
 display: grid ;
-grid-template-columns: repeat(4, 1fr) ;
+grid-template-columns: repeat(3, 1fr) ;
 row-gap: 2.5rem;
 margin-top: 3.5rem ;
 gap: 1.25rem
