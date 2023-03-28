58cdfe6c09be1534890a99e7fb0573dc5247af37
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-28 09:59:08 -0300

    arrumando pois estava meio a ordem dos componentes, agora joguei o Section Title e o FOrmpayment dentor do FormContainerPayment

--- src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentForm.tsx
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentForm.tsx
@@ -1,15 +1,10 @@
 import { SectionTitle } from "../SectionTitle";
 import { PaymentFormContainer } from "./style";
-import {  CurrencyDollar} from "phosphor-react";
+
 export function PaymentForm() {
   return (
     <PaymentFormContainer>
-      <SectionTitle
-        icon={<CurrencyDollar size={20} />}
-        title="Pagamento"
-        subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
-        
-      ></SectionTitle>
+    
     </PaymentFormContainer>
   );
 }
--- src/Pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,31 +1,43 @@
-import { MapPin } from "phosphor-react";
+import { CurrencyDollar, MapPin } from "phosphor-react";
 import { TitleText } from "../../../../Components/Header/Typography";
 import { FormSectionContainer } from "../../styles";
 import { SectionTitle } from "../SectionTitle";
-import { CompleteOrderFormContainer } from "./style";
+import { CompleteOrderFormContainer, PaymentFormContainer } from "./style";
 import { useTheme } from "styled-components";
 import { AdressForm } from "./AdressForm";
 import { PaymentForm } from "./PaymentForm";
 export function CompleteOrderForm() {
-  const colors = useTheme()
-    return (
+  const colors = useTheme();
+  return (
     <CompleteOrderFormContainer>
       <TitleText size="xs" color="subtitle">
         Complete seu pedido
       </TitleText>
-      <FormSectionContainer >
+      <FormSectionContainer>
         <SectionTitle
-          
-          icon={<MapPin size={22} weight="fill" color={colors.colors["brand-yellow-dark"]}/>}
+          icon={
+            <MapPin
+              size={22}
+              weight="fill"
+              color={colors.colors["brand-yellow-dark"]}
+            />
+          }
           title="Endereço de entrega"
           subtitle="Informe o endereço de entrega"
-          
         />
-        <AdressForm/>
-       
-
+        <AdressForm />
       </FormSectionContainer>
-      <PaymentForm/>
+      
+
+      <PaymentFormContainer>
+      <SectionTitle
+        icon={<CurrencyDollar size={20} />}
+        title="Pagamento"
+        subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+        
+      ></SectionTitle>
+        <PaymentForm />
+      </PaymentFormContainer>
     </CompleteOrderFormContainer>
   );
 }
