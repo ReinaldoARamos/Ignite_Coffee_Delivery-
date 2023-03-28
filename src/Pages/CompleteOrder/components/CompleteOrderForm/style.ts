170309ef48aa03aa184d79936d15ec8b9dcc4616
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-27 22:23:27 -0300

    terminando de configurar o grid controlando oque eu precisava

--- src/Pages/CompleteOrder/components/CompleteOrderForm/style.ts
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/style.ts
@@ -19,5 +19,18 @@ width: 100% ;
 display: grid ;
 grid-template-columns: 12.5rem 17.25rem 3.75rem ;
 row-gap: 1rem ;
-grid-auto-flow: dense
+grid-auto-flow: dense;
+
+.cep{
+    grid-column: span 3 ; //faz o cep ocupar 3 colunas
+    max-width: 12.5rem ;
+}
+
+.street{
+    grid-column: span 3 ;
+}
+
+.complement{
+    grid-column: span 2 ;
+}
 `
