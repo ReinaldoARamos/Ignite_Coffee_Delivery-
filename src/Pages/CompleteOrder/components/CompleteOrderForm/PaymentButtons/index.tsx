67be6f425db24101375e88b81676d4623c2943a5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-28 10:38:24 -0300

    aplicando a cor correta usando useTheme

--- src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentButtons/index.tsx
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentButtons/index.tsx
@@ -1,3 +1,16 @@
-export function PaymentButton() {
-        return(<button>Pagamento</button>)
+import { ReactNode } from "react"
+import { PaymentButtonStyle } from "./style"
+
+interface PayButtonProps{
+ icon: ReactNode,
+ text: string  
+}
+
+export function PaymentButton({icon, text} : PayButtonProps) {
+    
+        return(
+        <PaymentButtonStyle >
+           {icon} {text}
+        </PaymentButtonStyle>
+        )
 }
--- src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentForm.tsx
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentForm.tsx
@@ -1,13 +1,14 @@
-import { SectionTitle } from "../SectionTitle";
+import { Money } from "phosphor-react";
+import { useTheme } from "styled-components";
 import { PaymentButton } from "./PaymentButtons";
 import { PaymentFormContainer } from "./style";
 
 export function PaymentForm() {
+  const colors = useTheme();
   return (
     <PaymentFormContainer>
-    <PaymentButton />
-    <PaymentButton />
-    <PaymentButton />
+    <PaymentButton text="Dori" icon={<Money size={20} color={colors.colors["brand-purple-dark"]}/>}/>
+   
     </PaymentFormContainer>
   );
 }
