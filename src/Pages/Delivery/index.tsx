2c8d45d10f1a0167e63ae9f81b593a6aa58addc0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-29 16:13:50 -0300

    terminando de configurar os ícones

--- src/Pages/Delivery/index.tsx
+++ src/Pages/Delivery/index.tsx
@@ -6,7 +6,7 @@ import { IconContainer } from "../../Components/InfosWithIcons/styles";
 import { InfoWithIcon } from "../../Components/InfosWithIcons";
 import { Clock, CurrencyCircleDollar, MapPin } from "phosphor-react";
 export function Delivery() {
-    const {colors} = useTheme()
+  const { colors } = useTheme();
   return (
     <DeliveryContainer className="container">
       <div>
@@ -18,26 +18,35 @@ export function Delivery() {
 
       <section>
         <OrderDetailsContainer>
-            
-        <InfoWithIcon 
-          icon={<MapPin weight="fill"/>}
-          text="Entregue em Rua Zacarias, 433 "
-          iconBg={colors["brand-purple-dark"]}
-          
+          <InfoWithIcon
+            icon={<MapPin weight="fill" />}
+            text={
+              <RegularText size="s">
+                Entrega em Rua Zacarias Vieira, 102 <br />{" "}
+                <strong>São Pedro - Santa Catarina, SC</strong>
+              </RegularText>
+            }
+            iconBg={colors["brand-purple-dark"]}
           />
-    <InfoWithIcon 
-          icon={<Clock weight="fill"/>}
-          text="Previsão de entrega"
-          iconBg={colors["brand-yellow"]}
-          
+          <InfoWithIcon
+            icon={<Clock weight="fill" />}
+            text={
+              <RegularText size="s" color="subtitle">
+                Previsão de entrega <br /> <strong>20-30 minutos</strong>
+              </RegularText>
+            }
+            iconBg={colors["brand-yellow"]}
           />
-          <InfoWithIcon 
-          icon={<CurrencyCircleDollar weight="fill"/>}
-          text="Pagamento na entrega"
-          iconBg={colors["brand-yellow-dark"]}
-          
+          <InfoWithIcon
+            icon={<CurrencyCircleDollar weight="fill" />}
+            text={
+              <RegularText size="s" color="subtitle">
+                Forma de pagamento <br /> <strong>Cartão de crédito</strong>
+              </RegularText>
+            }
+            iconBg={colors["brand-yellow-dark"]}
           />
-       </OrderDetailsContainer>
+        </OrderDetailsContainer>
         <img src={GuyOnABike} />
       </section>
     </DeliveryContainer>
