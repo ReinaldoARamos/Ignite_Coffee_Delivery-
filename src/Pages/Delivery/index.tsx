284a19611344439ca1534af74ee00318e5e12068
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-29 14:37:15 -0300

    aplicando titleText

--- src/Pages/Delivery/index.tsx
+++ src/Pages/Delivery/index.tsx
@@ -1,5 +1,12 @@
+import { useTheme } from "styled-components";
+import { TitleText } from "../../Components/Header/Typography";
+import { DeliveryContainer } from "./style";
+
 export function Delivery() {
-    return(
-        <h1 className="container">Delivery</h1>
-    )
-}  
+  const colors = useTheme();
+  return (
+    <DeliveryContainer className="container">
+      <TitleText size="l">Ihu, seu pedido foi confirmado</TitleText>
+    </DeliveryContainer>
+  );
+}
--- src/Pages/Delivery/style.ts
+++ src/Pages/Delivery/style.ts
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const DeliveryContainer = styled.div`
+display: flex;
+margin-top: 5rem;
+flex-direction: column;
+gap: 2.5rem 
+//color: ${({theme}) => theme.colors["brand-yellow"]};
+`
