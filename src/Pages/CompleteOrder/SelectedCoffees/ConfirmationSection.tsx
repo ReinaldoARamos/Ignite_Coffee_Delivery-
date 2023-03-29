f5e2bed5954ddcf1d461151ec03134f45e203e03
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-03-29 11:16:11 -0300

    aplicando a tabela de total de itens e arrumando a estilização

--- src/Pages/CompleteOrder/SelectedCoffees/ConfirmationSection.tsx
+++ src/Pages/CompleteOrder/SelectedCoffees/ConfirmationSection.tsx
@@ -1,11 +1,24 @@
+import { RegularText } from "../../../Components/Header/Typography";
 import { ConfirmationSectionContainer } from "./style";
 
 export function ConfirmationSection() {
     
     return(
         <ConfirmationSectionContainer>
-            <div>Eae</div>
-            <div>Eae</div>
+            <div>
+                <RegularText size="s">Total de itens</RegularText>
+                <RegularText>R$: 9.90</RegularText>
+            </div>
+          
+            <div>
+                <RegularText size="s">Frete</RegularText>
+                <RegularText>R$: 29.90</RegularText>
+            </div>
+
+            <div>
+                <RegularText size="l" weight={700} color="subtitle">Total</RegularText>
+                <RegularText size="l" weight={700} color="subtitle" >R$: 3.50</RegularText>
+            </div>
         </ConfirmationSectionContainer>
     )
 }
--- src/Pages/CompleteOrder/SelectedCoffees/style.ts
+++ src/Pages/CompleteOrder/SelectedCoffees/style.ts
@@ -17,11 +17,12 @@ flex-direction: column;
 export const ConfirmationSectionContainer = styled.section`
 display: flex;
 flex-direction: column;
-gap: 0.08rem;
+gap: 0.8rem;
 
 >div{
     display: flex;
     align-items: center;
     justify-content: space-between;
+    
 }
 `
