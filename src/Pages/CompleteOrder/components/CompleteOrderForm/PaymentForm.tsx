5b54fa451613c384bebc471e37ed68ccb0cae5f9
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-01 20:48:10 -0300

    criando o objeto das formas de pagamento

--- src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentForm.tsx
+++ src/Pages/CompleteOrder/components/CompleteOrderForm/PaymentForm.tsx
@@ -1,8 +1,23 @@
-import { Money, CreditCard, Bank } from "phosphor-react";
+import { Money, CreditCard, Bank, CurrencyCircleDollar } from "phosphor-react";
 import { useTheme } from "styled-components";
 import { PaymentButton } from "../../components/PaymentButtons/index";
 import { PaymentButtonContainer, PaymentFormContainer } from "./style";
 
+
+const PaymentMethos = {
+  credit:{
+    label: "cartão de crédito",
+    icon: <CreditCard></CreditCard>
+  },
+  debit:{
+    label: "cartão de crédito",
+    icon: <Bank></Bank>
+  },
+  money:{
+    label: "cartão de crédito",
+    icon: <CurrencyCircleDollar></CurrencyCircleDollar>
+  },
+}
 export function PaymentForm() {
   const colors = useTheme();
   return (
