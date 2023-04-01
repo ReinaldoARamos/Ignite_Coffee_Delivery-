197d36146ead8c93d3b40602a88b8f9fe00417c9
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-01 18:57:21 -0300

    criando toda a base do zod validation schema e tipando os form

--- src/Pages/CompleteOrder/index.tsx
+++ src/Pages/CompleteOrder/index.tsx
@@ -4,18 +4,33 @@ import { COmpleteOrderContainer } from "./styles";
 import * as zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, FormProvider } from "react-hook-form";
+import { TypeOf } from "zod/lib";
 
 const confirmOrderFormValidationSchema = zod.object({
-    cep: zod.string()
-})
+  cep: zod.string(),  //objeto do zod onde fica o sccehema de valdiação do formulario
+});
 
+export type orderData = zod.infer<typeof confirmOrderFormValidationSchema>;  //zod infer infere o tipo de schema
+//em orderData
+
+type confirmOrderData = orderData; //ConfirmOrderData [e igual ao tipo inferido da schema para usar no form todo]
 export function CartPage() {
-    const confirmOrderForm = useForm({ 
-        resolver: zodResolver(confirmOrderFormValidationSchema)
-    })
+  const confirmOrderForm = useForm<confirmOrderData>({ //generic tipando  o confirmOrderFor
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  });
+
+  const { handleSubmit } = confirmOrderForm; //confirmOderForm que é um useForm com a tipagem com o schema
+  //passando o resolver da biblioteca zod pegando esse scehema
+  function handleConfirmOrder(data: confirmOrderData) {
+    console.log(data);
+  }
+
   return (
     <FormProvider {...confirmOrderForm}>
-      <COmpleteOrderContainer className="container">
+      <COmpleteOrderContainer
+        className="container"
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
         <CompleteOrderForm />
         <SelectedCoffees />
       </COmpleteOrderContainer>
