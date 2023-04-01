64a7662892c6d397224b0322f5be973a0319622e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-01 18:41:44 -0300

    importando zod e seus resolvers

--- src/Pages/CompleteOrder/index.tsx
+++ src/Pages/CompleteOrder/index.tsx
@@ -2,6 +2,8 @@
 import { CompleteOrderForm } from "./components/CompleteOrderForm";
 import { SelectedCoffees } from "./SelectedCoffees";
 import { COmpleteOrderContainer } from "./styles";
+import * as zod from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod'
 
 export function CartPage() {
  
