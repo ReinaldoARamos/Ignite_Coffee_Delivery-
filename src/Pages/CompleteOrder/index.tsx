9d0f5a02c1adb566b26236fb73f87d99fa636f01
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-01 18:48:32 -0300

    criando o formValidator, passando por volta do formulário, o zod objetct e desestruturando o schema

--- src/Pages/CompleteOrder/index.tsx
+++ src/Pages/CompleteOrder/index.tsx
@@ -1,18 +1,24 @@
-
 import { CompleteOrderForm } from "./components/CompleteOrderForm";
 import { SelectedCoffees } from "./SelectedCoffees";
 import { COmpleteOrderContainer } from "./styles";
-import * as zod from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod'
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, FormProvider } from "react-hook-form";
+
+const confirmOrderFormValidationSchema = zod.object({
+    cep: zod.string()
+})
 
 export function CartPage() {
- 
-    return(
-        <COmpleteOrderContainer className="container">
-
-            <CompleteOrderForm/>
-            <SelectedCoffees/>
-        
-        </COmpleteOrderContainer>
-        )
+    const confirmOrderForm = useForm({ 
+        resolver: zodResolver(confirmOrderFormValidationSchema)
+    })
+  return (
+    <FormProvider {...confirmOrderForm}>
+      <COmpleteOrderContainer className="container">
+        <CompleteOrderForm />
+        <SelectedCoffees />
+      </COmpleteOrderContainer>
+    </FormProvider>
+  );
 }
